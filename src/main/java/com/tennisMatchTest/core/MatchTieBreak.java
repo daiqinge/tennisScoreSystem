c598f24d4580a299378457a961eb60b9fd77a4cc
daiqinge <475285022@qq.com>
2019-12-02 09:12:28 +1100

    MatchTieBreak change

--- src/main/java/com/tennisMatchTest/core/MatchTieBreak.java
+++ src/main/java/com/tennisMatchTest/core/MatchTieBreak.java
@@ -3,14 +3,7 @@ package com.tennisMatchTest.core;
 import com.tennisMatchTest.bean.Player;
 
 public class MatchTieBreak extends AbstractMatch{
-//	public Player player1 = new Player("");
-//	public Player player2 = new Player("");
-//	
-//    public MatchTieBreak(Player player1, Player player2) {
-//    	this.player1 = player1;
-//		this.player2 = player2;
-//	}
-//    
+
 	public String score() {
 		if (playerSet[0].getScore() == GlobalVal.ZERO_POINT 
 				&& playerSet[1].getScore() == GlobalVal.ZERO_POINT) {
@@ -27,11 +20,15 @@ public class MatchTieBreak extends AbstractMatch{
 		if(playerSet[0].getScore() >= GlobalVal.SEVEN_POINTS
 				&&playerSet[0].getScore() - playerSet[1].getScore()>=GlobalVal.TWO_POINTS) {
 			scoreSet[0]++;
+			playerSet[0].setScore(0);
+			playerSet[1].setScore(0);
 			return playerSet[0];
 		}
 		if(playerSet[1].getScore() >= GlobalVal.SEVEN_POINTS
 				&&playerSet[1].getScore() - playerSet[0].getScore()>=GlobalVal.TWO_POINTS) {
 			scoreSet[1]++;
+			playerSet[0].setScore(0);
+			playerSet[1].setScore(0);
 			return playerSet[1];
 		}
 		return null;
