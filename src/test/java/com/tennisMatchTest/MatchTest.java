166427ee6548afb2c690ffac5b70c43f74ff11fc
daiqinge <475285022@qq.com>
2019-12-02 11:39:38 +1100

    MatchTest change

--- src/test/java/com/tennisMatchTest/MatchTest.java
+++ src/test/java/com/tennisMatchTest/MatchTest.java
@@ -16,11 +16,10 @@ public class MatchTest {
 	public void beforeGameTest() {
 		match = new Match(PLAYER1, PLAYER2);
 	}
-
+	
 	@Test
-	public void test_pointWon() {
-		match.pointWonBy(PLAYER1.getName());
-		match.pointWonBy(PLAYER2.getName());
+	public void test_zeroWon() {
+		assertEquals("0-0", match.score());
 	}
 
 	@Test
@@ -46,8 +45,8 @@ public class MatchTest {
 	
 	@Test
 	public void test_tieBreakWinscore() {
-		match.pointWonBy(PLAYER1.getName());
 		match.pointWonBy(PLAYER2.getName());
+		match.pointWonBy(PLAYER1.getName());
 		assertEquals("0-0, 15-15", match.score());
 		
 		match.pointWonBy(PLAYER1.getName());
@@ -94,6 +93,51 @@ public class MatchTest {
 		match.pointWonBy(PLAYER1.getName());
 		match.pointWonBy(PLAYER1.getName());
 		assertEquals("1-0", match.score()); 
+	}
+	
+	@Test
+	public void test_Player1Winscore() {
+		for(int i =0 ; i < 4; i++) {
+			match.pointWonBy(PLAYER1.getName());
+		}
+		assertEquals("1-0", match.score()); 
+		
+		match.pointWonBy(PLAYER1.getName());
+		assertEquals("1-0, 15-0", match.score()); 
+		
+		for(int i =0 ; i < 3; i++) {
+			match.pointWonBy(PLAYER1.getName());
+		}
+		assertEquals("2-0", match.score()); 
+	}
+	
+	@Test
+	public void test_Player2Winscore() {
+		for(int i =0 ; i < 4; i++) {
+			match.pointWonBy(PLAYER2.getName());
+		}
+		assertEquals("0-1", match.score()); 
+		
+		match.pointWonBy(PLAYER2.getName());
+		assertEquals("0-1, 0-15", match.score()); 
+		
+		for(int i =0 ; i < 4; i++) {
+			match.pointWonBy(PLAYER2.getName());
+		}
+		assertEquals("0-2, 0-15", match.score());
+	}
+	
+	@Test
+	public void test_MultiGamesWinscore() {
+		for(int i =0 ; i < 16; i++) {
+			match.pointWonBy(PLAYER1.getName());
+		}
+		assertEquals("4-0", match.score()); 
+		
+		for(int i =0 ; i < 21; i++) {
+			match.pointWonBy(PLAYER2.getName());
+		}
+		assertEquals("4-5, 0-15", match.score()); 
 	}
 
 }
